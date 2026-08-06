9d0f101df9736e3d0a233bb622e18380cb5657ee
naeemasultana0308-png <naeemasultana0308@gmail.com>
2026-08-06 12:18:05 +0530

    Add files via upload

---
+++ Vote.java
@@ -0,0 +1,12 @@
+public class Vote{
+  public static void main(String[]args){
+    byte age=21;
+    if(age>=18){
+      System.out.println("Eligible");
+    }
+    else{
+      System.out.println("Not Eligible");
+    }
+  }
+}
+   
